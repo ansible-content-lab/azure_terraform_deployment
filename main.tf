593fdede9f144e2e619ab3cc7f7e866ed9c8a295
Chris Ahl <cahl@redhat.com>
2023-12-11 17:07:19 -0500

    Init for DB on Azure using terraform (#1)
    * initial DB
    
    Signed-off-by: Chris Ahl <cahl@redhat.com>
    
    * more db changes
    
    Signed-off-by: Chris Ahl <cahl@redhat.com>
    
    * add DB extension for hstore
    
    Signed-off-by: Chris Ahl <cahl@redhat.com>
    
    * module for db
    
    Signed-off-by: Chris Ahl <cahl@redhat.com>
    
    * change for non-default location
    
    Signed-off-by: Chris Ahl <cahl@redhat.com>
    
    * newer azurerm provider version
    
    Signed-off-by: Chris Ahl <cahl@redhat.com>
    
    * changes based on reviews
    
    Signed-off-by: Chris Ahl <cahl@redhat.com>
    
    * changes based on reviews
    
    Signed-off-by: Chris Ahl <cahl@redhat.com>
    
    * changes based on reviews
    
    Signed-off-by: Chris Ahl <cahl@redhat.com>
    
    * changes based on reviews
    
    Signed-off-by: Chris Ahl <cahl@redhat.com>
    
    ---------
    
    Signed-off-by: Chris Ahl <cahl@redhat.com>

---
+++ main.tf
@@ -0,0 +1,105 @@
+locals {
+
+  create_deployment_id = var.deployment_id != "" ? 0 : 1
+  # Common tags to be assigned to all resources
+  persistent_tags = {
+    purpose = "automation"
+    environment   = "ansible-automation-platform"
+    deployment = "aap-infrastructure-${var.deployment_id}"
+  }
+}
+
+terraform {
+  required_version = ">= 1.5.4"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.83.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.6.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+#  subscription_id = "00000000-0000-0000-0000-000000000000"
+#  tenant_id       = "11111111-1111-1111-1111-111111111111"
+}
+
+resource "random_string" "deployment_id" {
+  count = local.create_deployment_id
+
+  length   = 8
+  special  = false
+  upper = false
+  numeric = false
+
+}
+
+resource "azurerm_resource_group" "aap" {
+  name     = var.resource_group
+  location = var.location
+  tags     = local.persistent_tags
+}
+
+resource "azurerm_virtual_network" "aap" {
+  name                = "${var.deployment_id}-aap-vn"
+  location            = azurerm_resource_group.aap.location
+  resource_group_name = azurerm_resource_group.aap.name
+  address_space       = [var.infrastructure_vpc_cidr]
+}
+
+resource "azurerm_subnet" "aap" {
+  name                 = "${var.deployment_id}-aap-sn"
+  resource_group_name  = azurerm_resource_group.aap.name
+  virtual_network_name = azurerm_virtual_network.aap.name
+  address_prefixes     = [var.infrastructure_vpc_subnet_cidr_postgres]
+  service_endpoints    = ["Microsoft.Storage"]
+  delegation {
+    name = "fs"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
+
+resource "azurerm_private_dns_zone" "aap" {
+  name                = "aap.${var.deployment_id}.postgres.database.azure.com"
+  resource_group_name = azurerm_resource_group.aap.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "aap" {
+  name                  = "postgres-link"
+  private_dns_zone_name = azurerm_private_dns_zone.aap.name
+  virtual_network_id    = azurerm_virtual_network.aap.id
+  resource_group_name   = azurerm_resource_group.aap.name
+}
+
+#
+# Database
+module "db" {
+  #depends_on = [random_string.deployment_id]
+  #depends_on = [azurerm_private_dns_zone.aap, azurerm_subnet.aap, azurerm_private_dns_zone_virtual_network_link.aap]
+  depends_on = [azurerm_private_dns_zone.aap, azurerm_subnet.aap]
+
+  source = "./modules/db"
+
+  deployment_id = var.deployment_id
+  resource_group = azurerm_resource_group.aap.name
+  location = azurerm_resource_group.aap.location
+  infrastructure_db_username = var.infrastructure_db_username
+  infrastructure_db_password = var.infrastructure_db_password
+  infrastructure_db_engine_version = var.infrastructure_db_engine_version
+  infrastructure_db_storage_mb = var.infrastructure_db_storage_mb
+  infrastructure_db_instance_sku = var.infrastructure_db_instance_sku
+  subnet_id = azurerm_subnet.aap.id
+  private_dns_zone_id = azurerm_private_dns_zone.aap.id
+}
---
+++ modules/db/variables.tf
@@ -0,0 +1,60 @@
+variable "resource_group" {
+  description = "Azure resource group."
+  type        = string
+  default     = "aap_on_azure"
+}
+
+variable "location" {
+  description = "Azure location."
+  type        = string
+  default     = "East US"
+}
+
+variable "deployment_id" {
+  description = "Creates a random string that will be used in tagging for correlating the resources used with a deployment of AAP."
+  type    = string
+  validation {
+    condition     = (length(var.deployment_id) == 8 || length(var.deployment_id) == 0) && (can(regex("^[a-z]", var.deployment_id)) || var.deployment_id == "")
+    error_message = "deployment_id length should be 8 chars and should contain lower case alpha chars only"
+  }
+}
+
+variable "infrastructure_db_username" {
+  description = "PostgreSQL username."
+  type        = string
+  default     = "psqladmin"
+}
+
+variable "infrastructure_db_password" {
+  description = "PostgreSQL password."
+  type        = string
+  sensitive   = true
+}
+
+variable "infrastructure_db_engine_version" {
+  description = "PostgreSQL DB version."
+  type        = string
+  default     = "13"
+}
+
+variable "infrastructure_db_storage_mb" {
+  description = "PostgreSQL DB storage in MB."
+  type        = number
+  default     = 32768
+}
+
+variable "infrastructure_db_instance_sku" {
+  description = "PostgreSQL DB instance SKU name."
+  type        = string
+  default     = "GP_Standard_D2s_v3"
+}
+
+variable "subnet_id" {
+  description = "Azure subnet id."
+  type        = string
+}
+
+variable "private_dns_zone_id" {
+  description = "Azure private DNS zone id."
+  type        = string
+}
